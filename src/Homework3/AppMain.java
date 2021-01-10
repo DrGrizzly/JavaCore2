f8f5212177c5f90275215045c8fa06433e47cafc
DrGrizzly45 <dr_grizzly@mail.ru>
2021-01-10 22:18:29 +0500

    Домашнее задание 3 (доработал первое)

--- src/Homework3/AppMain.java
+++ src/Homework3/AppMain.java
@@ -1,12 +1,44 @@
 package Homework3;
 
-public class AppMain {
-    String[] fruits = {"Яблоко", "Груша", "Мандарин", "Яблоко", "Персик",
-                      "Вишня", "Груша", "Банан", "Киви", "Абрикос"};
+import java.util.HashMap;
 
+public class AppMain {
+    private static String[] fruits = {"Яблоко", "Груша", "Мандарин", "Яблоко", "Персик",
+                                      "Вишня", "Груша", "Банан", "Киви", "Абрикос"};
+
+    private static HashMap<String, Integer> fruitHashMap = new HashMap<>();
+
+    private static void fillHashMap(){
+        for (String fruit : fruits) {
+            int count = fruitHashMap.computeIfAbsent(fruit, cnt -> 0);
+            fruitHashMap.put(fruit, ++count);
+        }
+    }
+
+    private static void printUnique(){
+        System.out.println("Вывод уникальных значений: ");
+        //Вывод в консоль уникальных слов
+        fruitHashMap.forEach((fruit, cnt) -> {
+            if (cnt == 1) {
+                System.out.println(fruit);
+            }
+        });
+    }
+
+    private static void printDuplicate(){
+        //Вывод одинаковых слов с количеством совпадений
+        fruitHashMap.forEach((fruit, cnt) -> {
+            if (cnt > 1) {
+                System.out.printf("Слово %s повторяется %d раз(а) %n", fruit, cnt);
+            }
+        });
+    }
 
     public static void main(String[] args) {
-
+        //Заполним список
+        fillHashMap();
+        printUnique();
+        printDuplicate();
         //Задание 2
         PhoneDict phonebook = new PhoneDict();
         phonebook.add("Валера", "+79125503300");
